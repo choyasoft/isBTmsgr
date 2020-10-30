f0743f526f793e2af77da531bc9519ca8951c814
Ivy <tarraconeros@gmail.com>
2020-10-30 21:48:41 +0100

    Añadido paired y available devices

---
+++ app/src/main/java/com/ivysoft/BTmessenger/DeviceListActivity.java
@@ -0,0 +1,46 @@
+package com.ivysoft.BTmessenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Set;
+
+public class DeviceListActivity extends AppCompatActivity {
+    private ListView listPairedDevices,listAvailableDevices;
+    private ArrayAdapter<String> adapterPairedDevices, adapterAvailableDevices;
+    private Context context;
+    private BluetoothAdapter bluetoothAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_list);
+        context = this;
+    }
+
+    private void init() {
+        listPairedDevices = findViewById(R.id.list_paired_devices);
+        listAvailableDevices = findViewById(R.id.list_available_devices);
+
+        adapterPairedDevices = new ArrayAdapter<String>(context, R.layout.device_list_item);
+        adapterAvailableDevices = new ArrayAdapter<String>(context, R.layout.device_list_item);
+
+        listPairedDevices.setAdapter(adapterPairedDevices);
+        listAvailableDevices.setAdapter(adapterAvailableDevices);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+        if (pairedDevices!=null && pairedDevices.size()>0){
+            for (BluetoothDevice device: pairedDevices){
+                adapterPairedDevices.add(device.getName()+ "\n" + device.getAddress());
+            }
+        }
+    }
+}
--- app/src/main/java/com/ivysoft/BTmessenger/MainActivity.java
+++ app/src/main/java/com/ivysoft/BTmessenger/MainActivity.java
@@ -2,9 +2,17 @@ package com.ivysoft.BTmessenger;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,7 +21,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 private Context context;
 private BluetoothAdapter bluetoothAdapter;
-
+private final int LOCATION_PERMISSION_REQUEST = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +54,7 @@ private BluetoothAdapter bluetoothAdapter;
         switch(item.getItemId()){
 
             case R.id.menu_search_devices:
+                checkPermissions();
                 Toast.makeText(context, "Buscando dispositivos...", Toast.LENGTH_SHORT).show();
                 return true;
 
@@ -57,18 +66,52 @@ private BluetoothAdapter bluetoothAdapter;
                 return super.onOptionsItemSelected(item);
         }
     }
-
+    // Método para comprobar los permisos necesarios (de Bluetooth)
     private void checkPermissions(){
-        
-    }
+        if(ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST );
 
-    private void enableBluetooth(){
-        if(bluetoothAdapter.isEnabled()) {
-            Toast.makeText(context, "Bluetooth ya estaba activado", Toast.LENGTH_SHORT).show();
-        } else {
-            bluetoothAdapter.enable();
-            Toast.makeText(context, "Activando Bluetooth...", Toast.LENGTH_SHORT).show();
+        }else{
+            Intent intent = new Intent(context, DeviceListActivity.class);
+            startActivity(intent);
         }
     }
 
-}
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(context, DeviceListActivity.class);
+                startActivity(intent);
+            } else {
+                new AlertDialog.Builder(context)
+                        .setCancelable(false)
+                        .setMessage("El permiso de localización es requerido.\n Por favor acepta el permiso.")
+                        .setPositiveButton("Permitir", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                checkPermissions();
+                            }
+                        })
+                        .setNegativeButton("Denegar", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                MainActivity.this.finish();
+                            }
+                        })
+                        .show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+         }
+        }
+        private void enableBluetooth () {
+            if (bluetoothAdapter.isEnabled()) {
+                Toast.makeText(context, "Bluetooth ya estaba activado", Toast.LENGTH_SHORT).show();
+            } else {
+                bluetoothAdapter.enable();
+                Toast.makeText(context, "Activando Bluetooth...", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
