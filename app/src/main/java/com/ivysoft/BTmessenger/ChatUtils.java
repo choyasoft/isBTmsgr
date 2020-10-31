9102644ee1d7631f6473e41d7580a19c5d2c0f22
Ivy <tarraconeros@gmail.com>
2020-10-31 16:41:28 +0100

    Métodos y excepciones de Connect, connectionFailed

--- app/src/main/java/com/ivysoft/BTmessenger/ChatUtils.java
+++ app/src/main/java/com/ivysoft/BTmessenger/ChatUtils.java
@@ -3,7 +3,9 @@ package com.ivysoft.BTmessenger;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 
 import java.io.IOException;
@@ -13,6 +15,8 @@ import java.util.UUID;
 public class ChatUtils {
     private Context context;
     private Handler handler;
+
+    private ConnectThread connectThread;
 
     private final UUID APP_UUID = UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66");
 
@@ -66,6 +70,60 @@ public class ChatUtils {
 
             socket = tmp;
         }
+        // Método que conecta y desconecta el socket
+        public void run(){
+            try{
+                socket.connect();
+            }catch(IOException e){
+                Log.e("Connect->AbreSocket", e.toString());
+                 try{
+                    socket.close();
+                     }catch (IOException e){
+                       Log.e("Connect->CierraSocket", e.toString());
 
+                }
+                 connectionFailed();
+                 return;
+            }
+
+            synchronized (ChatUtils.this){
+                connectThread = null;
+            }
+
+            connect(device);
+        }
+        // Método que cancela la conexión
+        public void cancel(){
+            try{
+                socket.close();
+
+            }catch (IOException e){
+                Log.e("Connect->Cancel", e.toString());
+            }
+        }
+    }
+
+    private synchronized void connectionFailed(){
+        Message message = handler.obtainMessage(MainActivity.MESSAGE_TOAST);
+        Bundle bundle = new Bundle();
+        bundle.putString(MainActivity.TOAST, "No se puede conectar");
+        message.setData(bundle);
+        handler.sendMessage(message);
+
+        ChatUtils.this.start();
+    }
+    // Método que realiza la conexión al dispositivo y cambia su estado a connected
+    private synchronized void connect(BluetoothDevice device){
+        if (connectThread!=null){
+            connectThread.cancel();
+            connectThread = null;
+        }
+        Message message = handler.obtainMessage(MainActivity.MESSAGE_DEVICE_NAME);
+        Bundle bundle = new Bundle();
+        bundle.putString(MainActivity.DEVICE_NAME, device.getName());
+        message.setData(bundle);
+        handler.sendMessage(message);
+
+        setState(STATE_CONNECTED);
     }
 }
